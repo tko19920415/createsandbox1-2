8ab4c075fadc35775e0028eea376e8760b0bca87
tko19920415 <100361329+tko19920415@users.noreply.github.com>
2022-02-25 00:25:07 +0900

    テンプレート文字列　＋　アロー関数について

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
-import "./styles.css";
+// テンプレート文字列に関して
+const name = "tko";
+const age = 29;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// 私の名前はtkoです。年齢は29歳です。　を出力
+
+// 今までの方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+console.log(message1);
+
+// テンプレート文字列を用いると
+const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
+
+// アロー関数
+
+//今までの関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数方式
+
+// 書き方 メジャー
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+// 書き方 省略
+// const func2 = str => {
+//    return str;
+// }
+// console.log(func2("func2です"));
+
+// 書き方 return 省略
+// const func2 = str => str;
+// console.log(func2("func2です"));
+
+// アロー関数足し算
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
